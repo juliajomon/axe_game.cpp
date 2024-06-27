1ad570b39b20885429684a697d35a9955cbe766a
Julia Jomon <juliajomon12@gmail.com>
2024-06-27 20:24:26 +0530

    Initial commit

---
+++ axe_game.cpp.cpp
@@ -0,0 +1,22 @@
+// axe_game.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "raylib.h"
+#include <iostream>
+
+int main()
+{
+    int width = 200;
+    int length = 300;
+    InitWindow(width,length, "Julia");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
